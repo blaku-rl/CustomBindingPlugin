a1150ceba7f2ce80102f231487bf7c13162b1e86
blakebam <blakebambico@gmail.com>
2021-02-14 02:46:22 -0600

    Added map for all the fucking keys. Logic for key presses is shown

--- CustomBindingPlugin/CustomBindingPlugin.cpp
+++ CustomBindingPlugin/CustomBindingPlugin.cpp
@@ -1,7 +1,6 @@
 #include "pch.h"
 #include "CustomBindingPlugin.h"
 
-
 BAKKESMOD_PLUGIN(CustomBindingPlugin, "write a plugin description here", plugin_version, PLUGINTYPE_FREEPLAY)
 
 std::shared_ptr<CVarManagerWrapper> _globalCvarManager;
@@ -9,40 +8,29 @@ std::shared_ptr<CVarManagerWrapper> _globalCvarManager;
 void CustomBindingPlugin::onLoad()
 {
 	_globalCvarManager = cvarManager;
-	//cvarManager->log("Plugin loaded!");
 
-	//cvarManager->registerNotifier("my_aweseome_notifier", [&](std::vector<std::string> args) {
-	//	cvarManager->log("Hello notifier!");
-	//}, "", 0);
+	gameWrapper->HookEvent("Function TAGame.GameViewportClient_TA.HandleKeyPress", std::bind(&CustomBindingPlugin::onTick, this, std::placeholders::_1));
 
-	//auto cvar = cvarManager->registerCvar("template_cvar", "hello-cvar", "just a example of a cvar");
-	//auto cvar2 = cvarManager->registerCvar("template_cvar2", "0", "just a example of a cvar with more settings", true, true, -10, true, 10 );
-
-	//cvar.addOnValueChanged([this](std::string cvarName, CVarWrapper newCvar) {
-	//	cvarManager->log("the cvar with name: " + cvarName + " changed");
-	//	cvarManager->log("the new value is:" + newCvar.getStringValue());
-	//});
-
-	//cvar2.addOnValueChanged(std::bind(&CustomBindingPlugin::YourPluginMethod, this, _1, _2));
-
-	// enabled decleared in the header
-	//enabled = std::make_shared<bool>(false);
-	//cvarManager->registerCvar("TEMPLATE_Enabled", "0", "Enable the TEMPLATE plugin", true, true, 0, true, 1).bindTo(enabled);
-
-	//cvarManager->registerNotifier("NOTIFIER", [this](std::vector<std::string> params){FUNCTION();}, "DESCRIPTION", PERMISSION_ALL);
-	//cvarManager->registerCvar("CVAR", "DEFAULTVALUE", "DESCRIPTION", true, true, MINVAL, true, MAXVAL);//.bindTo(CVARVARIABLE);
-	//gameWrapper->HookEvent("FUNCTIONNAME", std::bind(&TEMPLATE::FUNCTION, this));
-	//gameWrapper->HookEventWithCallerPost<ActorWrapper>("FUNCTIONNAME", std::bind(&CustomBindingPlugin::FUNCTION, this, _1, _2, _3));
-	//gameWrapper->RegisterDrawable(bind(&TEMPLATE::Render, this, std::placeholders::_1));
-
-
-	//gameWrapper->HookEvent("Function TAGame.Ball_TA.Explode", [this](std::string eventName) {
-	//	cvarManager->log("Your hook got called and the ball went POOF");
-	//});
-	// You could also use std::bind here
-	//gameWrapper->HookEvent("Function TAGame.Ball_TA.Explode", std::bind(&CustomBindingPlugin::YourPluginMethod, this);
+	ctrlKeyValue = gameWrapper->GetFNameIndexByString("LeftControl");
+	shiftKeyValue = gameWrapper->GetFNameIndexByString("LeftShift");
+	mKeyValue = gameWrapper->GetFNameIndexByString("M");
+	keysArePressed = false;
 }
 
 void CustomBindingPlugin::onUnload()
 {
+}
+
+void CustomBindingPlugin::onTick(std::string eventName)
+{
+	if (gameWrapper->IsKeyPressed(ctrlKeyValue) && gameWrapper->IsKeyPressed(shiftKeyValue) && gameWrapper->IsKeyPressed(mKeyValue) && !keysArePressed) {
+		keysArePressed = true;
+		cvarManager->log("All keys are pressed for the first time");
+		cvarManager->executeCommand("redirect_shoot");
+	}
+	
+	if ((!gameWrapper->IsKeyPressed(ctrlKeyValue) || !gameWrapper->IsKeyPressed(shiftKeyValue) || !gameWrapper->IsKeyPressed(mKeyValue)) && keysArePressed) {
+		keysArePressed = false;
+		cvarManager->log("One key is unpressed");
+	}
 }
